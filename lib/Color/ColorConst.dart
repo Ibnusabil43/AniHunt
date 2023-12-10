2ac979eecea5b274d5b1920d5219095bb1145dea
Ibnusabil43 <zaidan.697@gmail.com>
2023-12-10 13:50:45 +0700

    first commit

---
+++ lib/Color/ColorConst.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+import 'package:anihunt/Color/HextoColor.dart';
+
+final Color button1 = HexColor.fromHex('#FF7272');
+final Color button2 = HexColor.fromHex('#F53E3E');
+final Color secondary = HexColor.fromHex('#292929');
+final Color accent = HexColor.fromHex('#242424');
+final Color primary = HexColor.fromHex('#FFFFFF');
+final Color textDark = HexColor.fromHex('#000000');
+final Color textLight = HexColor.fromHex('#FFFFFF');
+final Color textSecondary = HexColor.fromHex('#BABABA');
