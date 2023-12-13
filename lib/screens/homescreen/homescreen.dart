43256df9be73901eb21d9da84b6fbf524e489e12
Himana <himana.on@gmail.com>
2023-12-13 16:43:18 +0700

    extract a widget called _NewRelease

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -29,93 +29,115 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 40, bottom: 10),
-                  child: Text(
-                    'New Release',
-                    style: TextStyle(
-                        color: accent,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.25,
-                  child: Stack(
-                    children: [
-                      CarouselSlider(
-                          items: _bannerWidgets,
-                          carouselController: _carouselController,
-                          options: CarouselOptions(
-                              autoPlay: true,
-                              height: double.infinity,
-                              autoPlayCurve: Curves.fastOutSlowIn,
-                              autoPlayAnimationDuration:
-                                  const Duration(milliseconds: 800),
-                              autoPlayInterval:
-                                  const Duration(milliseconds: 2500),
-                              viewportFraction: 1,
-                              onPageChanged: (index, reason) {
-                                setState(() {
-                                  carouselIndex = index;
-                                });
-                              })),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.navigate_before_rounded),
-                                iconSize:
-                                    MediaQuery.of(context).size.shortestSide *
-                                        0.12,
-                                onPressed: () =>
-                                    _carouselController.previousPage(),
-                                color: Color.fromRGBO(167, 173, 177, 1),
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.navigate_next_rounded),
-                                iconSize:
-                                    MediaQuery.of(context).size.shortestSide *
-                                        0.12,
-                                onPressed: () =>
-                                    _carouselController.previousPage(),
-                                color: Color.fromRGBO(167, 173, 177, 1),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: AnimatedSmoothIndicator(
-                            activeIndex: carouselIndex,
-                            count: _bannerWidgets.length,
-                            effect: const WormEffect(
-                              dotColor: Color.fromRGBO(217, 217, 217, 0.5),
-                              activeDotColor: Color.fromRGBO(217, 217, 217, 1),
-                              dotWidth: 30,
-                              dotHeight: 5,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            )
+            _NewRelease(
+                bannerWidgets: _bannerWidgets,
+                carouselController: _carouselController,
+                initialIndex: carouselIndex,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    carouselIndex = index;
+                  });
+                }),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _NewRelease extends StatefulWidget {
+  final int initialIndex;
+  final CarouselController carouselController;
+  final List<Widget> bannerWidgets;
+  final Function(int index, CarouselPageChangedReason reason) onPageChanged;
+  const _NewRelease(
+      {required this.bannerWidgets,
+      required this.carouselController,
+      required this.initialIndex,
+      required this.onPageChanged});
+
+  @override
+  State<_NewRelease> createState() => _NewReleaseState();
+}
+
+class _NewReleaseState extends State<_NewRelease> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 40, bottom: 10),
+          child: Text(
+            'New Release',
+            style: TextStyle(
+                color: accent, fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          child: Stack(
+            children: [
+              CarouselSlider(
+                  items: widget.bannerWidgets,
+                  carouselController: widget.carouselController,
+                  options: CarouselOptions(
+                    height: double.infinity,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 800),
+                    autoPlayInterval: const Duration(milliseconds: 2500),
+                    viewportFraction: 1,
+                    onPageChanged: widget.onPageChanged,
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.navigate_before_rounded),
+                        iconSize:
+                            MediaQuery.of(context).size.shortestSide * 0.12,
+                        onPressed: () =>
+                            widget.carouselController.previousPage(),
+                        color: const Color.fromRGBO(167, 173, 177, 1),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.navigate_next_rounded),
+                        iconSize:
+                            MediaQuery.of(context).size.shortestSide * 0.12,
+                        onPressed: () => widget.carouselController.nextPage(),
+                        color: const Color.fromRGBO(167, 173, 177, 1),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: AnimatedSmoothIndicator(
+                    activeIndex: widget.initialIndex,
+                    count: widget.bannerWidgets.length,
+                    effect: const WormEffect(
+                      dotColor: Color.fromRGBO(217, 217, 217, 0.5),
+                      activeDotColor: Color.fromRGBO(217, 217, 217, 1),
+                      dotWidth: 30,
+                      dotHeight: 5,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -124,7 +146,6 @@ class _BannerNewRelease extends StatelessWidget {
   final Anime anime;
   const _BannerNewRelease({
     required this.anime,
-    super.key,
   });
 
   @override
@@ -137,12 +158,12 @@ class _BannerNewRelease extends StatelessWidget {
         decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(Radius.circular(40)),
             image: DecorationImage(
-              colorFilter: const ColorFilter.mode(
-                Color.fromRGBO(0, 0, 0, 0.5),
-                BlendMode.darken,
-              ),
               image: AssetImage(anime.imageUrl),
               fit: BoxFit.cover,
+              colorFilter: const ColorFilter.mode(
+                Color.fromRGBO(0, 0, 0, 0.6),
+                BlendMode.darken,
+              ),
             )),
         child: Padding(
           padding: const EdgeInsets.only(right: 50, left: 50, bottom: 30),
@@ -156,12 +177,13 @@ class _BannerNewRelease extends StatelessWidget {
                   fontSize: 20,
                 ),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 anime.description,
-                overflow: TextOverflow.ellipsis,
+                // overflow: TextOverflow.fade,
                 textAlign: TextAlign.justify,
-                style: const TextStyle(
+                style: TextStyle(
+                  color: textSecondary,
                   fontSize: 10,
                 ),
               ),
